fce463adacec74cd5a6e212a0f34c4624de00403
Subin Saji <subinssaji@gmail.com>
2023-09-14 10:06:03 +0100

    addition of decorator examples with two @s

--- Decorators/decorators.py
+++ Decorators/decorators.py
@@ -1,6 +1,28 @@
+import functools
 def do_twice(func):
-    def wrapper_do_twice():
-        func()
-        func()
+    @functools.wraps(func)
+    def wrapper_do_twice(*args, **kwargs):
+        func(*args, **kwargs)
+        return func(*args, **kwargs)
     return wrapper_do_twice
 
+
+# decorating functions with parameters
+
+def smart_divide(func):
+    def inner(a,b):
+        print("I am going to divide", a , "and", b)
+        if b == 0 :
+            print("Cannot divide!")
+            return
+        
+        return func(a,b)
+    return inner
+
+@smart_divide
+def divide(a,b):
+    print(a/b)
+    
+
+divide(2,5)
+divide(2,0)
--- Decorators/importing_decorator.py
+++ Decorators/importing_decorator.py
@@ -1,8 +1,24 @@
 from decorators import do_twice
 
+#@do_twice
+#def say_whee():
+#    print("Whee")
+    
+    
+#@do_twice
+#def greet(name):
+#    print(f"Hello {name}")
+    
+#greet("Hello")
+
+
 @do_twice
-def say_whee():
-    print("Whee")
-    
-    
-say_whee()
+def return_greeting(name):
+    print("Creating greeting")
+    return f"hi {name}"
+
+hi_adam = return_greeting("Adam")
+print(hi_adam)
+
+
+
