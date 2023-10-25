dfeba9ad5e7a39bac138bc1689914257d916a3c8
Subin Saji <subinssaji@gmail.com>
2023-10-25 09:38:30 +0100

    moved to correct folders

---
+++ pytest/test_functions_parametrize.py
@@ -0,0 +1,55 @@
+import pytest
+
+from pytest.examples import (
+    addition,
+    substraction,
+    reverse_string,
+    capitalise_string,
+    clean_string,
+)
+
+
+# tests for simple maths
+
+@pytest.mark.parametrize( "a, b, expected", [(1,2,3), (5,-1,4),],)
+def test_addition(a,b, expected):
+    assert addition(a, b) == expected
+    
+    
+@pytest.mark.parametrize(" a, b, expected", [(1,2,-1), (5,-1,6),],)
+def test_substraction(a,b, expected):
+    assert substraction(a, b) == expected
+    
+    
+# tests for simple strings    
+    
+@pytest.mark.parametrize( "string, expected", 
+    [
+        ("hello", "olleh")
+        ("world", "dlrow")
+        
+        ],)
+def test_reverse_string(a,b, expected):
+    assert reverse_string(a, b) == expected
+    
+    
+@pytest.mark.parametrize( "string, expected", 
+    [
+        ("hello", "HELLO")
+        ("world", "WORLD")
+        
+        ],)
+def test_capitalise_string(string, expected):
+    assert capitalise_string(string) == expected
+    
+    
+
+@pytest.mark.parametrize( "string, expected", 
+    [
+        ("hello", "HELLO")
+        ("world", "WORLD")
+        
+        ],)
+def test_clean_string(string, expected):
+    assert clean_string(string) == expected
+    
