1d0177a9e115106d6165df4414a8bff523dfaada
Subin Saji <subinssaji@gmail.com>
2023-10-25 10:22:18 +0100

    fixed tests

--- py_testing/test_functions_parametrize.py
+++ py_testing/test_functions_parametrize.py
@@ -7,47 +7,47 @@ from examples import (addition,
                       )
 
 
-# tests for simple maths
-
-@pytest.mark.parametrize( "a, b, expected", [(1,2,3), (5,-1,4),],)
-def test_addition(a,b, expected):
-    assert addition(a, b) == expected
-    
-    
-@pytest.mark.parametrize(" a, b, expected", [(1,2,-1), (5,-1,6),],)
-def test_substraction(a,b, expected):
-    assert subtraction(a, b) == expected
-    
-    
-# tests for simple strings    
-    
-@pytest.mark.parametrize( "string, expected", 
-    [
-        ("hello", "olleh"),
-        ("world", "dlrow"),
-        
-        ],)
-def test_reverse_string(a,b, expected):
-    assert reverse_string(a, b) == expected
-    
-    
-@pytest.mark.parametrize( "string, expected", 
-    [
-        ("hello", "HELLO"),
-        ("world", "WORLD"),
-        
-        ],)
-def test_capitalise_string(string, expected):
-    assert capitalise_string(string) == expected
-    
-    
-
-@pytest.mark.parametrize( "string, expected", 
-    [
-        ("hello", "HELLO"),
-        ("world", "WORLD"),
-        
-        ],)
-def test_clean_string(string, expected):
-    assert clean_string(string) == expected
-    
+@pytest.mark.parametrize( "a, b, expected",  
+    [  
+        (1, 2, 3),  
+        (5, -1, 4),  
+    ],)  
+def test_addition(a, b, expected):  
+    assert addition(a, b) == expected  
+  
+  
+@pytest.mark.parametrize( "a, b, expected",  
+    [  
+        (1, 2, -1),  
+        (5, -1, 6),  
+    ],)  
+def test_subtraction(a, b, expected):  
+    assert subtraction(a, b) == expected  
+  
+  
+# Test String Functions  
+@pytest.mark.parametrize( "string, expected",  
+    [  
+        ("hello", "olleh"),  
+        ("world", "dlrow"),  
+    ],)  
+def test_reverse_string(string, expected):  
+    assert reverse_string(string) == expected  
+  
+  
+@pytest.mark.parametrize( "string, expected",  
+    [  
+        ("hello", "HELLO"),  
+        ("world", "WORLD"),  
+    ],)  
+def test_capitalize_string(string, expected):  
+    assert capitalise_string(string) == expected  
+  
+  
+@pytest.mark.parametrize( "string, expected",  
+    [  
+        (" hello ", "hello"),  
+        (" WoRlD ", "world"),  
+    ],)  
+def test_clean_string(string, expected):  
+    assert clean_string(string) == expected  
