d5b81a85f4421b7703a4a86f4e9a4e8bf22b6a83
Subin Saji <subinssaji@gmail.com>
2023-10-25 09:14:16 +0100

    cleaned up via indendation and nameing

--- test_functions_parametrize.py
+++ test_functions_parametrize.py
@@ -11,45 +11,45 @@ from examples import (
 
 # tests for simple maths
 
-@pytest.mark.parametrize("a, b, expected", [(1,2,3), (5,-1,4),],)
+@pytest.mark.parametrize( "a, b, expected", [(1,2,3), (5,-1,4),],)
 def test_addition(a,b, expected):
     assert addition(a, b) == expected
     
     
-@pytest.mark.parametrize("a, b, expected", [(1,2,-1), (5,-1,6),],)
+@pytest.mark.parametrize(" a, b, expected", [(1,2,-1), (5,-1,6),],)
 def test_substraction(a,b, expected):
     assert substraction(a, b) == expected
     
     
 # tests for simple strings    
     
-@pytest.mark.parametrize("string, expected", 
-                         [
-                             ("hello", "olleh")
-                             ("world", "dlrow")
-                             
-                             ],)
+@pytest.mark.parametrize( "string, expected", 
+    [
+        ("hello", "olleh")
+        ("world", "dlrow")
+        
+        ],)
 def test_reverse_string(a,b, expected):
     assert reverse_string(a, b) == expected
     
     
-@pytest.mark.parametrize("string, expected", 
-                         [
-                             ("hello", "HELLO")
-                             ("world", "WORLD")
-                             
-                             ],)
+@pytest.mark.parametrize( "string, expected", 
+    [
+        ("hello", "HELLO")
+        ("world", "WORLD")
+        
+        ],)
 def test_capitalise_string(string, expected):
-    assert reverse_string(string) == expected
+    assert capitalise_string(string) == expected
     
     
 
-@pytest.mark.parametrize("string, expected", 
-                         [
-                             ("hello", "HELLO")
-                             ("world", "WORLD")
-                             
-                             ],)
+@pytest.mark.parametrize( "string, expected", 
+    [
+        ("hello", "HELLO")
+        ("world", "WORLD")
+        
+        ],)
 def test_clean_string(string, expected):
-    assert reverse_string(string) == expected
+    assert clean_string(string) == expected
     
